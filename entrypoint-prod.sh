4f261a06adf3ed72a1577ead61b711b63b84acc1
Миша <mika2386@mail.ru>
2026-01-27 02:35:47 +0300

    Update entrypoint-prod.sh

--- entrypoint-prod.sh
+++ entrypoint-prod.sh
@@ -20,7 +20,7 @@ start_django() {
     GUNICORN_TIMEOUT=${GUNICORN_TIMEOUT:-30}
 
     echo "Starting server..."
-    gunicorn myproject.wsgi:application
+    gunicorn bot_manager.wsgi:application
     #gunicorn --timeout "$GUNICORN_TIMEOUT" --workers 3 --threads 3 --bind 0.0.0.0:"$PORT" config.wsgi:application
 
     echo "Server has started!"
